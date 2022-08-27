ce8a6b34f452beb0ef4be03e28ac82f45de673e8
rafa3012new <92983803+rafa3012new@users.noreply.github.com>
2022-08-27 10:12:58 -0400

    Add files via upload

---
+++ SQL/prueba.sql
@@ -0,0 +1,24 @@
+-- create database dojo2022;
+ use dojo2022;
+-- create table usuarios (
+-- id int auto_increment not null primary key,
+-- nombre varchar(100),
+-- apellido varchar(100),
+-- email varchar(100)
+-- );
+
+-- insert into usuarios (nombre,apellido,email) values ("Rafael","Fernandez","rafael_fernandez@gmail.com");
+-- insert into usuarios (nombre,apellido,email) values ("Bruce","Wayne","brucewayne@gmail.com");
+-- insert into usuarios (nombre,apellido,email) values ("Clark","Kent","clarkkent@gmail.com");
+-- insert into usuarios (nombre,apellido,email) values ("Barry","Allen","barryallen@gmail.com");
+-- insert into usuarios (nombre,apellido,email) values ("Daiana","Prince","dianaprince@gmail.com");
+-- insert into usuarios (nombre,apellido,email) values ("Victor","Stone","victorstonew@gmail.com");
+
+
+
+
+-- update usuarios set email = "rafaelfernandez@gmail.com" where nombre = 'Rafael';
+
+-- delete from usuarios where nombre = "Rafael";
+
+SELECT * FROM usuarios;
